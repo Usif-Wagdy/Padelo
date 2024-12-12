dab68f0ae55166da73c3497b5b70d4eb5c1c8028
Mo7amed7osam <mido.hoss2003@gmail.com>
2024-12-12 22:29:13 +0200

    Remove user listing endpoint for security reasons.

--- BackEnd/controllers/user.controller.js
+++ BackEnd/controllers/user.controller.js
@@ -1,17 +1,6 @@
 const User = require('../models/user.model');
 const bcrypt = require('bcrypt');
 
-exports.getUsers = async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json({ users });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: 'Error fetching users', error });
-  }
-};
-
 exports.addUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
