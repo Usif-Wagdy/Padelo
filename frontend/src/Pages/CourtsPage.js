3cfe7eab3b315e819c95d31ac2001049dd22f695
Mina Lotfy <minalotfy867@gmail.com>
2024-12-19 00:30:46 +0200

    Add dynamic navigation from Courts to Reservation page

--- frontend/src/Pages/CourtsPage.js
+++ frontend/src/Pages/CourtsPage.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Header from "../Components/Header";
-// import { courts } from "../Components/courtsData";
 import "../Styles/Courts.css";
 import defaultCourtImage from "../assets/OIP.jpg";
 
 function Courts() {
   const [searchQuery, setSearchQuery] = useState("");
   const [courts, setCourts] = useState([]);
+  const navigate = useNavigate();
 
+  // Fetch all courts from the API
   useEffect(() => {
     fetch("http://127.0.0.1:3000/api/courts")
       .then((response) => response.json())
@@ -17,6 +19,7 @@ function Courts() {
       .catch((error) => console.error("Error fetching courts:", error));
   }, []);
 
+  // Filter courts based on search query
   const filteredCourts = searchQuery
     ? courts.filter((court) => {
         const query = searchQuery.trim().toLowerCase();
@@ -26,6 +29,11 @@ function Courts() {
         );
       })
     : courts;
+
+  // Navigate to the reservation page with the selected court's by it's id
+  const handleCourtClick = (courtId) => {
+    navigate(`/reservation/${courtId}`);
+  };
 
   return (
     <div className="courts-page">
@@ -55,10 +63,16 @@ function Courts() {
         </div>
       </div>
 
+      {/* Courts List */}
       <div className="courts-container">
         {filteredCourts.length > 0 ? (
           filteredCourts.map((court) => (
-            <div key={court.__id} className="Courts-court-card">
+            <div
+              key={court._id} 
+              className="Courts-court-card"
+              onClick={() => handleCourtClick(court._id)}
+              style={{ cursor: "pointer" }} // Make cards clickable
+            >
               <img
                 src={court.image || defaultCourtImage}
                 alt={court.name}
