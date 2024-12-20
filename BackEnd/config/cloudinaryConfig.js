16fc44f8358cdbe2e891977dadf720c44778c940
Mo7amed7osam <mido.hoss2003@gmail.com>
2024-12-20 16:39:41 +0200

    Huge updategit add . Frontend can now upload files, not just URLs.

---
+++ BackEnd/config/cloudinaryConfig.js
@@ -0,0 +1,25 @@
+const cloudinary = require('cloudinary').v2;
+const {
+  CloudinaryStorage,
+} = require('multer-storage-cloudinary');
+const multer = require('multer');
+
+// Configure Cloudinary
+cloudinary.config({
+  cloud_name: 'daxm65zyi',
+  api_key: '137132137829925',
+  api_secret: 'QaueEe7IdbkMZP8FGgTY0kSbuuo',
+});
+
+// Set up Multer with Cloudinary storage
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'user_photos', // Cloudinary folder
+    allowed_formats: ['jpeg', 'png', 'jpg'], // Accepted file types
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = { cloudinary, upload }; // Export both `cloudinary` and `upload`
